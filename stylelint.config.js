a46adf9e7092d0410f1b4b4da5c3d9035dc61deb
rogermedico <roger.medico@gmail.com>
2020-10-14 18:26:50 +0200

    responsive done

--- stylelint.config.js
+++ stylelint.config.js
@@ -21,7 +21,9 @@ module.exports = {
           "screen",
           "use",
           "mixin",
-          "include"
+          "include",
+          "function",
+          "return"
         ]
       }
     ]
